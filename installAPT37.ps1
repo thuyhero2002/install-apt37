e5338299c898cac6440fe9abd708406479a30d69
thuyhero2002 <79744581+thuyhero2002@users.noreply.github.com>
2024-01-07 10:40:24 +0700

    Update installAPT37.ps1
    vẫn phải sửa lại

--- installAPT37.ps1
+++ installAPT37.ps1
@@ -2,13 +2,13 @@ $downloadDir = "C:\AtomicRedTeam\atomics"
 
 $repoUrl = "https://github.com/thuyhero2002/install-apt37/archive/refs/heads/main.zip"
 $zipFile = "$downloadDir\APT37.zip"
-$extractedDir = "$downloadDir\APT37"
 
 Write-Host "Downloading repository..." -ForegroundColor Cyan
 Invoke-WebRequest -Uri $repoUrl -OutFile $zipFile
 
 Write-Host "Extracting repository..." -ForegroundColor Cyan
-Expand-Archive -Path $zipFile -DestinationPath $extractedDir
+Expand-Archive -Path $zipFile -DestinationPath $downloadDir
+Rename-Item -Path "$downloadDir\install-apt37-main" -NewName "APT37"
 
 Remove-Item $zipFile
 
